28d92d681af34ddbbd11606ff0f163baca345a89
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-06-08 12:52:45 +0200

    Adding a list of standard countries

--- src/components/CountrySelect/CountryFilter.tsx
+++ src/components/CountrySelect/CountryFilter.tsx
@@ -7,3 +7,31 @@ export const EU: Array<string> = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE'
 export const NO: Array<string> = ['NO']
 export const NORDIC: Array<string> = ['DK', 'FI', 'FO', 'GL', 'IS', 'NO', 'SE']
 export const SCANDINAVIA: Array<string> = ['DK', 'SE', 'NO']
+
+//https://www.worldometers.info/geography/alphabetical-list-of-countries/
+export const STANDARD: Array<string> = [
+  'AD', 'AE', 'AF', 'AG', 'AL', 'AM', 'AO', 'AR', 'AT', 'AU', 'AZ',
+  'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BN', 'BO','BR', 'BS', 'BT', 'BY', 'BW', 'BZ',
+  'CA', 'CD', 'CF', 'CG', 'CH', 'CI', 'CL', 'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CY', 'CZ',
+  'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ',
+  'EC', 'EE', 'EG', 'ER', 'ES', 'ET',
+  'FI', 'FJ', 'FM', 'FR',
+  'GA', 'GD', 'GE', 'GH', 'GM', 'GN', 'GQ', 'GT', 'GW', 'GY',
+  'HN', 'HT', 'HR','HU',
+  'ID', 'IE',  'IL', 'IN', 'IQ', 'IR', 'IS', 'IT',
+  'JM', 'JO', 'JP',
+  'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW', 'KZ',
+  'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY',
+  'MA', 'MC', 'MD', 'ME', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MR', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ',
+  'NA', 'NE', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NZ',
+  'OM',
+  'PA', 'PE','PG', 'PH', 'PK', 'PL', 'PS', 'PT' , 'PW', 'PY',
+  'QA',
+  'RO', 'RS', 'RU', 'RW',
+  'SA', 'SB', 'SC', 'SD', 'SE', 'SG', 'SI', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SY', 'SZ',
+  'TD', 'TG', 'TH', 'TJ', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TZ',
+  'UA', 'UG', 'UK', 'US', 'UY', 'UZ',
+  'VA', 'VC', 'VE', 'VN', 'VU',
+  'WS',
+  'YE',
+  'ZA','ZM', 'ZW']
